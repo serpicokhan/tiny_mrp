07b114de974f2aa8f833706b72def3e4f22b80f6
moein <morteza20413100@gmail.com>
2024-04-23 20:43:21 +0330

    fds
    fds

--- tiny_mrp/mrp/models/failure.py
+++ tiny_mrp/mrp/models/failure.py
@@ -3,7 +3,7 @@ from django.db import models
 class Failure(models.Model):
     code = models.CharField("کد",max_length=50)
     name = models.CharField("نام",max_length=100)
-    is_it_count=models.BooleanField("فعال در محسابات",default=False)
+    is_it_count=models.BooleanField("فعال در محسابات",default=True)
 
     def __str__(self):
         return f"{self.code} - {self.name}"
