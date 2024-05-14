f32ce6a51598d6d566c5ce86f3914945f652267f
moeinserpico <81881855+serpicokhan@users.noreply.github.com>
2024-05-14 18:30:36 +0330

    dsa
    das

--- tiny_mrp/mrp/static/project/dash.js
+++ tiny_mrp/mrp/static/project/dash.js
@@ -102,12 +102,16 @@ var draw_line_asset_production=function(start_dt,end_dt,machine,category){
                     type: 'area',
                     height: 400,
                     
+                    
                 },
                 fill: {
                     colors: [ '#9C27B0']
                   },
                 dataLabels: {
                     enabled: true,
+                    offsetX: -20,
+                   
+                    hideOverflow: true,
                     style: {
                       fontSize: "22px",
                       fontFamily: "Helvetica, Arial, sans-serif",
