e119c14b571b5c48808fa37288241c5eebc0b59e
moein <morteza20413100@gmail.com>
2024-11-12 23:31:13 +0330

    fdsfds
    fds

--- tiny_mrp/mrp/templatetags/custom_filters.py
+++ tiny_mrp/mrp/templatetags/custom_filters.py
@@ -18,3 +18,6 @@ def get_rank(dictionary, current_key):
     sorted_keys = sorted(dictionary, key=dictionary.get, reverse=True)
     # Find the rank of the current item based on its position in sorted keys
     return rank[sorted_keys.index(current_key)]  # Adding 1 to start rank at 1 instead of 0
+@register.filter
+def get_sum_item(dictionary):
+    return sum(dictionary.values())
