2dd9a4984f265db218bd6f65ff9e8e336646f549
moein <morteza20413100@gmail.com>
2025-01-17 21:11:47 +0330

    Update settings.py

--- tiny_mrp/tiny_mrp/settings.py
+++ tiny_mrp/tiny_mrp/settings.py
@@ -138,3 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+TIME_ZONE = 'Asia/Tehran'
+USE_TZ = True
