6922b017b92722f1c508c0eca9bc3d28cb7572b1
moein sarvi <81881855+serpicokhan@users.noreply.github.com>
2025-07-19 08:12:36 +0330

    dsa
    das

--- tiny_mrp/mrp/models/users.py
+++ tiny_mrp/mrp/models/users.py
@@ -5,7 +5,38 @@ from django.contrib.auth.models import User
 import jdatetime
 from django.utils.timezone import now
 
+class SysUser(models.Model):
+    def __str__(self):
+        return "{}".format(self.fullName)
+    def get_userStatus(self):
+                 if(self.userStatus==True):
+                     return "<i class='fa fa-play'></i>								"
+                 else:
+                     return "<i class='fa fa-stop'></i>"
+    def getName(self):
 
+        xxxx=UserGroups.objects.filter(userUserGroups=self.id)
+        st=[]
+        for i in xxxx:
+            st.append(i.groupUserGroups)
+        # print(''.join(str(e) for e in st))
+        return '<br/>'.join(str(e) for e in st)
+
+
+    Dashboard=1
+    WorkOrderAssignedToMe=2
+    MessageCenterInbox=3
+    WorkOrders=4
+    Location=(
+        (Dashboard,'داشبورد'),
+        (WorkOrderAssignedToMe,'درخواستهای انتسابی به من'),
+        (MessageCenterInbox,'صندوق ورودی پیامها'),
+        (WorkOrders,'درخواست'),
+    )
+    userId = models.OneToOneField(User, on_delete=models.CASCADE,null=True,blank=True)
+    password=models.CharField(max_length=20)
+    token=models.CharField(max_length=20,null=True,blank=True)
+    fullName=models.CharField("مشخصات کامل",max_length = 50)
  
     personalCode=models.CharField("کد پرسنلی",max_length = 50)
     title=models.CharField("عنوان",max_length = 50,null=True,blank=True)
